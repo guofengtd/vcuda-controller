bff4f845285320b179d6524e8840911994600a22
xgf <xiong.guofeng@ocloudware.com>
2022-02-09 13:01:38 +0800

    werror

--- src/loader.c
+++ src/loader.c
@@ -1125,7 +1125,7 @@ void dump_files(const char *folder) {
       fprintf(stderr,"cannot open directory: %s\n", folder);
       return;
   }
-  (void) chdir(folder);
+  chdir(folder);
   while((entry = readdir(dp)) != NULL) {
       lstat(entry->d_name,&statbuf);
       if(S_ISDIR(statbuf.st_mode)) {
